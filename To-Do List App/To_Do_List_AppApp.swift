a6aa9c613f304d5fb4c7271225cde5e54b222300
Audrey Ng <audrey518@gmail.com>
2023-08-15 07:48:03 -0700

    Initial Commit

---
+++ To-Do List App/To_Do_List_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  To_Do_List_AppApp.swift
+//  To-Do List App
+//
+//  Created by Audrey Ng on 8/15/23.
+//
+
+import SwiftUI
+
+@main
+struct To_Do_List_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
